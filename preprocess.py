407a9ada7c718871028cbde39a11b2ddc6ad2455
ab93 <avikbasu93@gmail.com>
2016-04-15 20:02:33 -0700

    returns length of doc

--- preprocess.py
+++ preprocess.py
@@ -4,36 +4,38 @@ import nltk.data
 from nltk.corpus import stopwords
 
 def readData(filename,lang='spanish'):
-    sentList = []
-    with open(filename,'r') as fp:
-        for line in fp:
-            line = line.strip()
-            sentList = cleanLine(line,sentList,lang)
+	sentList = []
+	lenDoc = 0
+	with open(filename,'r') as fp:
+		for line in fp:
+			line = line.strip()
+			sentList, lenDoc = cleanLine(line,sentList,lenDoc,lang)
 
-    return sentList
+	return sentList,lenDoc
 
 
-def cleanLine(line,finalList,lang):
-    try:
-        tokenizer = nltk.data.load('tokenizers/punkt/'+ lang + '.pickle')
-    except:
-        "language not supported!!"
-        return None
+def cleanLine(line,finalList,lenDoc,lang):
+	try:
+		tokenizer = nltk.data.load('tokenizers/punkt/'+ lang + '.pickle')
+	except:
+		"language not supported!!"
+		return None
 
-    line = line.decode('utf-8').lower()
-    notNum = re.compile(r'[0-9]+')
-    line = notNum.sub('',line)
-    words = line.split(' ')
-    impWords = filter(lambda x: x not in stopwords.words(lang), words)
-    line = ' '.join(impWords)
+	line = line.decode('utf-8').lower()
+	notNum = re.compile(r'[0-9]+')
+	line = notNum.sub('',line)
+	words = line.split(' ')
+	impWords = filter(lambda x: x not in stopwords.words(lang), words)
+	line = ' '.join(impWords)
 
-    sentList = tokenizer.tokenize(line)
+	sentList = tokenizer.tokenize(line)
 
-    for sent in sentList:
-        words = re.findall(r'\w+', sent,flags = re.UNICODE | re.LOCALE)
-        finalList.append(words)
+	for sent in sentList:
+		words = re.findall(r'\w+', sent,flags = re.UNICODE | re.LOCALE)
+		finalList.append(words)
+		lenDoc += len(words)
 
-    return finalList
+	return finalList, lenDoc
 
-#readData('data/2010-2013/2010-13c0.txt','english')
+#readData('data/data/2010-2013/2010-13c0.txt')
 #readData('englishdata/reuters0.txt','english')
