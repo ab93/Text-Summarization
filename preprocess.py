d5c20959ca791a46f7f30ee42dd7de742d77a381
ab93 <avikbasu93@gmail.com>
2016-04-16 00:08:54 -0700

    returns unfiltered sentList

--- preprocess.py
+++ preprocess.py
@@ -5,24 +5,30 @@ from nltk.corpus import stopwords
 
 def readData(filename,lang='spanish'):
 	'''
-	Return the [ s1[w1,w2,w3..], s2[w1,w2,w3..] ...] and length of doc
+	Return the [ s1[w1,w2,w3..], s2[w1,w2,w3..] ...], unfiltered sentence list
+	and length of doc.
 	'''
-	sentList = []
+	sentWordList = []
+	sentList = []     #Unfiltered sentence list
 	lenDoc = 0
 	with open(filename,'r') as fp:
 		for line in fp:
 			line = line.strip()
-			sentList, lenDoc = cleanLine(line,sentList,lenDoc,lang)
+			sentWordList, sentList, lenDoc = cleanLine(line,sentWordList,sentList,lenDoc,lang)
 
-	return sentList,lenDoc
+	return sentWordList,sentList,lenDoc
 
 
-def cleanLine(line,finalList,lenDoc,lang):
+def cleanLine(line,finalList,sentList,lenDoc,lang):
 	try:
 		tokenizer = nltk.data.load('tokenizers/punkt/'+ lang + '.pickle')
 	except:
 		"language not supported!!"
 		return None
+
+	tempList = tokenizer.tokenize(line.decode('utf-8'))
+	for sent in tempList:
+		sentList.append(sent)
 
 	line = line.decode('utf-8').lower()
 	notNum = re.compile(r'[0-9]+')
@@ -31,15 +37,15 @@ def cleanLine(line,finalList,lenDoc,lang):
 	impWords = filter(lambda x: x not in stopwords.words(lang), words)
 	line = ' '.join(impWords)
 
-	sentList = tokenizer.tokenize(line)
+	sentWordList = tokenizer.tokenize(line)
 
-	for sent in sentList:
+	for sent in sentWordList:
 		words = re.findall(r'\w+', sent,flags = re.UNICODE | re.LOCALE)
 		finalList.append(words)
 		lenDoc += len(words)
 
-	return finalList, lenDoc
+	return finalList, sentList, lenDoc
 
 
-#readData('data/data/2010-2013/2010-13c0.txt')
+readData('data/data/2010-2013/2010-13c0.txt')
 #readData('englishdata/reuters0.txt','english')
